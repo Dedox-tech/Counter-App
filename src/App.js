71398d74c8a69e26647041357d0664a359cd2a1e
Diego Murillo <diegomurilloferrer@gmail.com>
2022-05-20 18:45:36 -0500

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container'
+import { Card } from '@mui/material';
+import { CardContent } from '@mui/material';
+import { Button } from "@mui/material";
+import { Stack } from '@mui/material';
+import Image from "./images/waves-image.svg";
+import { useState } from 'react';
 
 function App() {
+
+  const imageStyles = {
+    backgroundImage: `url(${Image})`,
+    backgroundSize: "cover",
+    minHeight: "100vh",
+  };
+
+  const [myCount, setMyCount] = useState(0);
+
+  function increaseCount () {
+    setMyCount(
+      function (previousCount) {
+        return previousCount + 1;
+      }
+    );
+  }
+
+  function decreaseCount () {
+    setMyCount(
+      function (previousCount) {
+        return previousCount - 1;
+      }
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction="row"
+      justifyContent ="center"
+      alignItems="center"
+      alignContent="center"
+      wrap="nowrap"
+      style={imageStyles}
+    >
+      <Grid item>
+        <Container style = {{maxWidth: "400px"}}>
+          <Card elevation = {5}>
+            <CardContent sx = {{p:3}}>
+              <Typography variant="h4" color="initial" mb = {2}> React Counter App </Typography>
+              <Typography paragraph> This small React App let you manage a basic counter. Feel free to explore the functionality clicking in the buttons below. </Typography>
+              <Typography paragraph fontWeight = "fontWeightBold"> Counter: {myCount} </Typography>
+              <Stack spacing = {1} direction = "row" justifyContent="flex-end">
+                <Button variant = "contained" onClick = {increaseCount}> Increase </Button>
+                <Button variant = "outlined"  onClick = {decreaseCount}> Decrease </Button>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
+      </Grid>
+    </Grid>
   );
 }
 
